418ee6cc7ae286378a6a349535db41109fbc2403
Ripe Jalapeno <cscotsman18@gmail.com>
2024-09-06 08:40:11 -0500

    Added welcome banner

---
+++ scripts/home.js
@@ -0,0 +1,5 @@
+const weeklyAdLink = document.querySelector('.js-weekly-ad-link');
+
+weeklyAdLink.addEventListener('click', () => {
+  window.open('https://www.chrisfoodcenter.com/_files/ugd/a8f83f_57f31f24e65542c69e3b49718bcff6ae.pdf', '_blank');
+})
