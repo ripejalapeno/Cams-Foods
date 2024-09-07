20dececdaafa52b29d630ac7874a3e57e3fb0396
Ripe Jalapeno <cscotsman18@gmail.com>
2024-09-07 07:45:51 -0500

    Added links to navigation section

--- scripts/home.js
+++ scripts/home.js
@@ -3,3 +3,27 @@ const weeklyAdLink = document.querySelector('.js-weekly-ad-link');
 weeklyAdLink.addEventListener('click', () => {
   window.open('https://www.chrisfoodcenter.com/_files/ugd/a8f83f_57f31f24e65542c69e3b49718bcff6ae.pdf', '_blank');
 })
+
+const meatProcessingLink = document.querySelector('.js-meat-processing-link');
+
+meatProcessingLink.addEventListener('click', () => {
+  window.open('https://www.chrisfoodcenter.com/_files/ugd/a8f83f_87a12b9b919842969aaaed4d6c31ffd9.pdf', '_blank');
+})
+
+const applicationLink = document.querySelector('.js-application-link');
+
+applicationLink.addEventListener('click', () => {
+  window.open('https://www.chrisfoodcenter.com/_files/ugd/a8f83f_df944cbf21c44cf699cc73293c81a9db.pdf', '_blank');
+})
+
+const deliLink = document.querySelector('.js-deli-link');
+
+deliLink.addEventListener('click', () => {
+  window.open('https://www.chrisfoodcenter.com/menu', '_blank');
+})
+
+const partyPlanningLink = document.querySelector('.js-party-planning-link');
+
+partyPlanningLink.addEventListener('click', () => {
+  window.open('https://www.chrisfoodcenter.com/_files/ugd/a8f83f_8fb4bc5e9da748c39fde3bc0180b0bcb.pdf', '_blank');
+})
