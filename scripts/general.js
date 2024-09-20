88b04c1636cc5b6516ef36f0449b6859de3d7f7f
Ripe Jalapeno <cscotsman18@gmail.com>
2024-09-19 21:36:51 -0500

    Various tweaks to font size and box models

--- scripts/general.js
+++ scripts/general.js
@@ -1,7 +1,7 @@
 export function resizeMap() {
   if (window.innerWidth <= 768) {
     googleMap.width = '250';
-    googleMap.height = '125';
+    googleMap.height = '250';
   } else if (window.innerWidth > 768 && window.innerWidth <= 1000) {
     googleMap.width = '500';
     googleMap.height = '250';
